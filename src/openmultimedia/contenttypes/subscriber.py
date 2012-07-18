3c4b6a8e661227fda761ae3074fbb5a09eb3a0a1
Franco Pellegrini <frapell@gmail.com>
2012-07-18 19:24:41 -0300

    Fixes para el subscriber

--- src/openmultimedia/contenttypes/subscriber.py
+++ src/openmultimedia/contenttypes/subscriber.py
@@ -9,7 +9,7 @@ from zope.lifecycleevent.interfaces import IObjectModifiedEvent
 
 from five import grok
 
-from plone.namedfile.field import NamedImage
+from plone.namedfile import NamedImage
 
 from openmultimedia.api.interfaces import IVideoAPI
 from openmultimedia.contenttypes.content.video import IVideo
@@ -20,32 +20,34 @@ from openmultimedia.contenttypes.content.video import IVideo
 def update_metadata(obj, event):
     """ Read metadata associated with the video from the OpenMultimedia API.
     """
-    video_api = getUtility(IVideoAPI)
-    json = video_api.get_json(obj.remote_url)
+    
+    if obj.remote_url:
+        video_api = getUtility(IVideoAPI)
+        json = video_api.get_json(obj.remote_url)
 
-    if json:
-        title = json.get('titulo', None)
-        description = json.get('descripcion', None)
-        slug = json.get('slug', None)
-        thumbnail = json.get('thumbnail_grande', None)
-        video_url = json.get('archivo_url', None)
-        audio_url = json.get('audio_url', None)
+        if json:
+            title = json.get('titulo', None)
+            description = json.get('descripcion', None)
+            slug = json.get('slug', None)
+            thumbnail = json.get('thumbnail_grande', None)
+            video_url = json.get('archivo_url', None)
+            audio_url = json.get('audio_url', None)
 
-        if title:
-            obj.title = title
+            if title:
+                obj.title = title
 
-        if description:
-            obj.description = description
+            if description:
+                obj.description = description
 
-        if slug:
-            obj.slug = slug
+            if slug:
+                obj.slug = slug
 
-        if thumbnail:
-            data = urllib2.urlopen(thumbnail)
-            obj.image = NamedImage(data.read())
+            if thumbnail:
+                data = urllib2.urlopen(thumbnail)
+                obj.image = NamedImage(data.read(), filename=thumbnail)
 
-        if video_url:
-            obj.video_url = video_url
+            if video_url:
+                obj.video_url = video_url
 
-        if audio_url:
-            obj.audio_url = audio_url
+            if audio_url:
+                obj.audio_url = audio_url
