234ec0075598dec0829c3e81a51d967cd4c41d4b
Gonzalo Almeida <flecox@gmail.com>
2012-07-31 16:40:39 -0300

    termianndo tipo de contenido galleria

--- src/openmultimedia/contenttypes/browser/gallery.py
+++ src/openmultimedia/contenttypes/browser/gallery.py
@@ -1,8 +1,13 @@
+from datetime import datetime
+
+from zope.component import getUtility
+
 from five import grok
 
 from openmultimedia.contenttypes.content.gallery import IGallery
 
 from collective.nitf.browser import View as NITFView
+from collective.prettydate.interfaces import IPrettyDate
 
 grok.templatedir('gallery_templates')
 
@@ -13,6 +18,36 @@ class View(NITFView):
     grok.name('view')
     grok.require('zope2.View')
     grok.template('gallery')
+    
+    def get_media(self):
+        """ Return a list of object brains inside the NITF object.
+        """
+        media_types = ['Image']
+
+        return self._get_brains(media_types)
+
+    def get_prettydate(self, obj):
+        date_utility = getUtility(IPrettyDate)
+
+        date = None
+        if datetime > date:
+            date = obj.created()
+        else:
+            date = obj.effective()
+
+        return date_utility.date(date)
+
+    def media_type(self, obj):
+        media = ''
+        if IATImage.providedBy(obj):
+            media = 'image'
+
+        return media
+
+    @property
+    def force_column(self):
+        return True
+    
 
 class Media(NITFView):
     grok.context(IGallery)
