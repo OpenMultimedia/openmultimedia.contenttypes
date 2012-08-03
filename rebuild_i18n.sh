db9601a7f745935fffdab73020fd8aa5ef0b0930
Gonzalo Almeida <flecox@gmail.com>
2012-08-03 12:46:50 -0300

    acomodando el rebuild

--- rebuild_i18n.sh
+++ rebuild_i18n.sh
@@ -5,14 +5,14 @@ I18NDOMAIN="openmultimedia.contenttypes"
 BASE_DIRECTORY="src/openmultimedia/contenttypes"
 
 # Synchronise the templates and scripts with the .pot.
-../../bin/i18ndude rebuild-pot --pot ${BASE_DIRECTORY}/locales/${I18NDOMAIN}.pot \
+i18ndude rebuild-pot --pot ${BASE_DIRECTORY}/locales/${I18NDOMAIN}.pot \
     --merge ${BASE_DIRECTORY}/locales/manual.pot \
     --create ${I18NDOMAIN} \
     ${BASE_DIRECTORY}
 
 # Synchronise the resulting .pot with all .po files
 for po in ${BASE_DIRECTORY}/locales/*/LC_MESSAGES/${I18NDOMAIN}.po; do
-    ../../bin/i18ndude sync --pot ${BASE_DIRECTORY}/locales/${I18NDOMAIN}.pot $po
+    i18ndude sync --pot ${BASE_DIRECTORY}/locales/${I18NDOMAIN}.pot $po
 done
 
 # Synchronise the templates and scripts with the .pot.
@@ -23,8 +23,8 @@ i18ndude rebuild-pot --pot ${BASE_DIRECTORY}/locales/plone.pot \
 
 # Synchronise the plone's pot file (Used for the workflows)
 for po in ${BASE_DIRECTORY}/locales/*/LC_MESSAGES/plone.po; do
-    ../../bin/i18ndude sync --pot ${BASE_DIRECTORY}/locales/plone.pot $po
+    i18ndude sync --pot ${BASE_DIRECTORY}/locales/plone.pot $po
 done
 
 # Report of errors and suspect untranslated messages
-../../bin/i18ndude find-untranslated ${BASE_DIRECTORY}
+i18ndude find-untranslated ${BASE_DIRECTORY}
