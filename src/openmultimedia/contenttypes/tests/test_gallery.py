0f3c219f4a5df87cb33b717e0fe08d355aecc414
Gonzalo Almeida <flecox@gmail.com>
2012-08-17 15:57:44 -0300

    agregando tests

--- src/openmultimedia/contenttypes/tests/test_gallery.py
+++ src/openmultimedia/contenttypes/tests/test_gallery.py
@@ -4,6 +4,7 @@ import unittest2 as unittest
 
 from zope.component import createObject
 from zope.component import queryUtility, queryMultiAdapter
+from zope.interface import directlyProvides
 
 from plone.dexterity.interfaces import IDexterityFTI
 from plone.uuid.interfaces import IAttributeUUID
@@ -12,6 +13,8 @@ from plone.app.referenceablebehavior.referenceable import IReferenceable
 
 from plone.app.testing import TEST_USER_ID
 from plone.app.testing import setRoles
+
+from collective.nitf.interfaces import INITFLayer
 
 from openmultimedia.contenttypes.content.gallery import IGallery
 from openmultimedia.contenttypes.testing import INTEGRATION_TESTING
@@ -24,13 +27,15 @@ class ContentTypeTestCase(unittest.TestCase):
     def setUp(self):
         self.portal = self.layer['portal']
         self.request = self.layer['request']
+        directlyProvides(self.request, INITFLayer)
 
         setRoles(self.portal, TEST_USER_ID, ['Manager'])
         self.portal.invokeFactory('Folder', 'test-folder')
         setRoles(self.portal, TEST_USER_ID, ['Member'])
         self.folder = self.portal['test-folder']
 
-        self.folder.invokeFactory('openmultimedia.contenttypes.gallery', 'g1')
+        self.folder.invokeFactory('openmultimedia.contenttypes.gallery', 
+            'g1', description="gallery desctiption")
         self.g1 = self.folder['g1']
 
     def test_adding(self):
@@ -54,7 +59,32 @@ class ContentTypeTestCase(unittest.TestCase):
     def test_is_referenceable(self):
         self.assertTrue(IReferenceable.providedBy(self.g1))
         self.assertTrue(IAttributeUUID.providedBy(self.g1))
+    
+    def test_image_description(self):
+        """
+        Here we test that calling image.Description() on an image that doesn't 
+        have a description. doesn't bring the parent description.
+        """
+        self.g1.invokeFactory('Image', id="image1", description="image description test")
+        self.g1.invokeFactory('Image', id="image2")
+        image1 = self.g1['image1']
+        image2 = self.g1['image2']
+        self.assertTrue(image1.Description() == "image description test")
+        self.assertTrue(image2.Description() == "")
+        #its diferent from the parent description
+        self.assertTrue(image2.Description() != "gallery desctiption")
 
     def test_view(self):
         view = queryMultiAdapter((self.g1, self.request), name='view')
-        self.assertTrue(view is not None)
+        self.assertTrue(view is not None)      
+    
+    def test_view_get_description(self):
+        self.g1.invokeFactory('Image', id="image1", description="image description test")
+        self.g1.invokeFactory('Image', id="image2")
+        image1 = self.g1['image1']
+        image2 = self.g1['image2']
+        view = queryMultiAdapter((self.g1, self.request), name='view')
+        image1_desc = view.get_description(image1)
+        image2_desc = view.get_description(image2)
+        self.assertTrue(image1_desc == "image description test")
+        self.assertTrue(image2_desc == "gallery desctiption")
